9b151455d5d3d1cbbd6e91fa3c5c70e4186b7430
Drew Conly <atconly@gmail.com>
2016-07-16 13:57:40 -0600

    set up routes

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
-angular.module('speakCode', [ngRoute]);
+angular.module('speakCode', ['ngRoute']);
+
+// Routes
 
 angular.module('speakCode').config(function ($routeProvider) {
     $routeProvider.when( '/', {
@@ -9,13 +11,76 @@ angular.module('speakCode').config(function ($routeProvider) {
     });
     $routeProvider.when( '/about', {
         templateUrl: '/views/about.html',
-        controller : 'aboutCtrl',
+        controller : 'aboutCtrl'
+    });
+    $routeProvider.when( '/game', {
+        templateUrl: '/views/game.html',
+        controller : 'gameCtrl'
     });
 
     // the default route
     $routeProvider
       .otherwise({
-        redirectTo: '/about'
+        redirectTo: '/main'
       });
-  })
-;
+});
+
+// Navigation Controller
+
+angular.module('speakCode')
+  .controller('NavCtrl', ['$scope', '$location', navCtrl])
+
+function navCtrl( $scope, $location ) {
+    var nc = this;
+
+    $scope.$on('$routeChangeSuccess', function() {
+        nc.locationPath = $location.path();
+        console.log('locationPath: ' + nc.locationPath );
+    });
+
+}
+
+// About controller
+
+angular.module('speakCode')
+    .controller('aboutCtrl', aboutCtrl)
+
+function aboutCtrl() {
+  var about = this;
+
+  about.text = 'Joe London is a Pirate';
+}
+
+// code Controller
+
+angular.module('speakCode')
+  .controller('codeCtrl', codeCtrl);
+
+function codeCtrl() {
+  var code = this;
+
+  code.banner = "Speaking in Code";
+  code.button = 'Begin Game';
+}
+
+// Game Controller
+
+angular.module('speakCode')
+  .controller('gameCtrl', gameCtrl);
+
+function gameCtrl() {
+  var game = this;
+
+  game.banner = 'Welcome to the ThunderDome!!';
+}
+
+// Setup Controller
+
+angular.module('speakCode')
+  .controller('setupCtrl', setupCtrl);
+
+function setupCtrl() {
+  var setup = this;
+
+  setup.text = "Moderator, please enter team names"; 
+}
--- scripts/codeCtrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('speakCode')
-  .controller('codeCtrl', codeCtrl);
-
-function codeCtrl() {
-  var code = this;
-
-  code.banner = "Speaking in Code";
-  code.button = 'Begin Game';
-}
