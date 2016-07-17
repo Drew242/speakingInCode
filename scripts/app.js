3e48afb42be41164b79e0a1b0e5a3f206661fcb3
Drew Conly <atconly@gmail.com>
2016-07-16 21:04:23 -0600

    make random function work on factories

--- scripts/app.js
+++ scripts/app.js
@@ -40,7 +40,7 @@ function navCtrl( $scope, $location ) {
 
 }
 
-// Team Factory
+// Factories
 
 angular.module('speakCode')
   .factory('teamFactory', teamFactory)
@@ -57,16 +57,84 @@ function teamFactory() {
       team2 = data;
     }
   }
-
   function get() {
     return team1, team2;
   }
-
   return {
     set: set,
     get: get
   }
+}
 
+angular.module('speakCode')
+  .factory('easyProblems', easyProblems)
+
+function easyProblems() {
+
+  function rando(data) {
+    return data[Math.floor(Math.random()*data.length)];
+  };
+
+  var problems = [{
+    problem: 'var fruits = ["Banana", "Orange", "Apple", "Mango"];document.getElementById("demo").innerHTML = fruits;'
+  }, {
+    problem: 'function myFunction() {fruits.pop();document.getElementById("demo").innerHTML = fruits;}'
+  }, {
+    problem: 'function myFunction() {var text = "";var i;for (i = 0; i < 5; i++) {text += "The number is " + i + "<br>";}document.getElementById("demo").innerHTML = text;}'
+  }];
+
+  // rando(problems);
+
+  return {
+    problems: problems,
+    rando: rando
+  }
+}
+
+angular.module('speakCode')
+  .factory('mediumProblems', mediumProblems)
+
+function mediumProblems() {
+
+  function rando(data) {
+    return data[Math.floor(Math.random()*data.length)];
+  };
+
+  var problems = [{
+    problem: 'module.service("userService", function(){this.users = ["John", "James", "Jake"];});'
+  }, {
+    problem: 'module.factory("userService", function(){var fac = {};fac.users = ["John", "James", "Jake"];return fac;});'
+  }, {
+    problem: 'app.controller("CalculatorController", function($scope, CalculatorService) {$scope.doSquare = function() {$scope.answer = CalculatorService.square($scope.number);}$scope.doCube = function() {$scope.answer = CalculatorService.cube($scope.number);}});'
+  }]
+
+  return {
+    problems: problems,
+    rando: rando
+  }
+}
+
+angular.module('speakCode')
+  .factory('hardProblems', hardProblems)
+
+function hardProblems() {
+
+  function rando(data) {
+    return data[Math.floor(Math.random()*data.length)];
+  };
+
+  var problems = [{
+    problem: '$scope.changeColor = function(person, bool) {if(bool === true) {$scope.personColour = {color: "#"+person.colour};} else if (bool === false) {$scope.personColour = {color: "white"}; //or, whatever the original color is}};'
+  }, {
+    problem: '<ul ng-repeat="bday in bdays"><li><span ng-hide="editing" ng-click="editing = true">{{bday.name}} | {{bday.date}}</span><form ng-show="editing" ng-submit="editing = false"><label>Name:</label><input type="text" ng-model="bday.name" placeholder="Name" ng-required/><label>Date:</label><input type="date" ng-model="bday.date" placeholder="Date" ng-required/><br/><button class="btn" type="submit">Save</button><a class="btn" ng-click="remove()">Delete</a></form></li></ul>'
+  }, {
+    problem: 'app.controller("MyCtrl", function($scope) {$scope.modalShown = false;$scope.toggleModal = function() {$scope.modalShown = !$scope.modalShown;};});'
+  }]
+
+  return {
+    problems: problems,
+    rando: rando
+  }
 }
 
 // About controller
@@ -97,19 +165,35 @@ function codeCtrl() {
 angular.module('speakCode')
   .controller('gameCtrl', [
     'teamFactory',
+    'easyProblems',
+    'mediumProblems',
+    'hardProblems',
     gameCtrl
   ]);
 
-function gameCtrl(teamFactory) {
+function gameCtrl(teamFactory, easyProblems, mediumProblems, hardProblems) {
   var game = this;
 
-  game.banner    = 'Welcome to the ThunderDome!!';
-  game.grabTeams = teamFactory.get();
+  game.banner        = 'Welcome to the ThunderDome!!';
+  game.grabTeams     = teamFactory.get();
+  game.randomProblem = "This is where a code snippet will go";
 
   game.pointPrompt = function(i) {
     console.log('points clicked');
     var points = prompt('How many points??');
     document.getElementById('score-' + i).innerHTML = points
+  }
+
+  game.easy = function() {
+    game.randomProblem = easyProblems.rando(easyProblems.problems);
+  }
+
+  game.medium = function() {
+    game.randomProblem = mediumProblems.rando(mediumProblems.problems);
+  }
+
+  game.hard = function() {
+    game.randomProblem = hardProblems.rando(hardProblems.problems);
   }
 }
 
